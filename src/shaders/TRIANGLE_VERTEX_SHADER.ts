223f4d26d603abd2c1d0cd26f206a677d6a29e71
Blazej Kustra <blazej.kustra@swmansion.com>
2025-10-25 17:35:56 +0200

    refactor: improve formatting and structure of TRIANGLE_VERTEX_SHADER for clarity

--- src/shaders/TRIANGLE_VERTEX_SHADER.ts
+++ src/shaders/TRIANGLE_VERTEX_SHADER.ts
@@ -1,6 +1,11 @@
 export const TRIANGLE_VERTEX_SHADER = /* wgsl */ `
-  struct VSOut { @builtin(position) pos: vec4<f32>, @location(0) ndc: vec2<f32> };
-  @vertex fn main(@builtin(vertex_index) vid: u32) -> VSOut {
+  struct VSOut { 
+    @builtin(position) pos: vec4<f32>,
+    @location(0) ndc: vec2<f32>,
+  };
+  
+  @vertex 
+  fn main(@builtin(vertex_index) vid: u32) -> VSOut {
     var p = array<vec2<f32>,3>(
       vec2<f32>(-1.0,-3.0), vec2<f32>(-1.0,1.0), vec2<f32>(3.0,1.0)
     );
