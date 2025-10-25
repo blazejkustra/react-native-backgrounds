ac32a30759baefd21fb2ce9ee2fd9ced464697c2
Blazej Kustra <blazej.kustra@swmansion.com>
2025-10-25 10:57:00 +0200

    Initialize GPU 🗿🚀

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,8 @@
+import { initGPU } from './utils/initGPU';
+
+// Need to initialize the GPU before using the library
+initGPU();
+
 import CircularGradient from './components/CircularGradient';
 
 export { CircularGradient };
